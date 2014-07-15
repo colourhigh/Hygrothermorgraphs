ef1104aeff2d81b5e60146b1d79553005c583712
joshgagnon <josh.n.gagnon@gmail.com>
2014-07-15 18:58:02 +1200

    Update main.js
    Added temp and temp_tween
    To use,
    temp 20 3 // approximate 20 degrees for 3 minutes
    temp_tween 10 30 15 // tween from 10 degrees to 30 degrees over 15 minutes.
    TODO, add ordered [temp, on, off] triples to the temp_map array at top main.js.

--- Ether_Relay2/main.js
+++ Ether_Relay2/main.js
@@ -1,132 +1,12 @@
 $(document).ready(function() {
-    var coords = [];
-    var width = 700;
-    var height = 200;
-    var margin = {
-        top: 30,
-        right: 30,
-        bottom: 30,
-        left: 30
-    };
-
-
-    var lineFunc = d3.svg.line()
-        .x(function(d) {
-
-            return d.x;
-        })
-        .y(function(d) {
-            return d.y;
-        })
-        .interpolate("linear");
-
-    var xAxis = d3.svg.axis()
-        .scale(d3.scale.linear().range([0, width]))
-        .orient("bottom")
-        .ticks(5);
-    var yAxis = d3.svg.axis()
-        .scale(d3.scale.linear().range([height, 0]))
-        .orient("left")
-        .ticks(5);
-
-    var update_draw = function() {
-        coords = [];
-        d3.selectAll('.point')
-            .each(function(c) {
-                coords.push({
-                    x: this.x,
-                    y: this.y
-                });
-            });
-        coords.sort(function(a, b) {
-            return a.x - b.x;
-        });
-        svgContainer.selectAll('.path')
-            .data([coords]) // set the new data
-        .attr("d", lineFunc)
-    }
-
-    var click = function() {
-        if (d3.event.defaultPrevented) return;
-        var point = d3.mouse(this),
-            p = {
-                x: point[0],
-                y: point[1]
-            }
-        svgContainer.append("circle")
-            .attr("transform", "translate(" + p.x + "," + p.y + ")")
-            .attr("r", "6")
-            .attr('class', 'point')
-            .attr("stroke", "blue")
-            .attr("stroke-width", 1)
-            .attr("fill", "#3388ff")
-            .property({
-                x: p.x,
-                y: p.y
-            })
-            .style("cursor", "pointer")
-            .call(drag);
-        update_draw();
-    }
-
-
-
-    var svgContainer = d3.select("body")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .style('padding', 30)
-        .on('click', click)
-
-
-    svgContainer.selectAll('.path')
-        .data([
-            []
-        ])
-        .enter().append('path')
-        .attr('class', 'path')
-        .attr('d', lineFunc)
-        .attr("stroke", "blue")
-        .attr("stroke-width", 2)
-        .attr("fill", "none");
-    svgContainer.append("g")
-        .attr("transform", "translate(0," + (height) + ")")
-        .attr({
-            fill: 'none',
-            stroke: 'black'
-        }).call(xAxis).selectAll('text')
-        .attr({
-            fill: 'black',
-            stroke: 'none'
-        });
-
-    svgContainer.append("g")
-        .attr("transform", "translate(0, " + 0 + ")")
-        .attr({
-            fill: 'none',
-            stroke: 'black'
-        }).call(yAxis).selectAll('text')
-        .attr({
-            fill: 'black',
-            stroke: 'none'
-        })
-
-
-    var drag = d3.behavior.drag()
-        .on("drag", dragmove);
-
-    function dragmove(d) {
-        var d3this = d3.select(this)
-        var index = d3this.attr('index') | 0;
-        var x = d3.event.x;
-        var y = d3.event.y;
-        d3this.attr("transform", "translate(" + x + "," + y + ")")
-            .property({
-                x: x,
-                y: y
-            });
-        update_draw();
-    }
+ 
+	var temp_map = [
+		[16.75, 1.2, 4],
+		[25.1, 1, 4],
+		[37.2, 2, 2],
+		[38.7, 3, 4],	
+		[44.0, 4, 2]
+	];
 
 
 
@@ -139,6 +19,8 @@ $(document).ready(function() {
     var stack = [];
     var pc = 0;
     var source_map = [];
+    var simulation = [];
+
 
     var update = function(state) {
         var xhr;
@@ -168,32 +50,31 @@ $(document).ready(function() {
         output.html(nums.join('<br/>'));;
     }
 
-    function loop() {
-        console.log(pc, program[pc]);
+    function loop(next, send) {
         switch (program[pc++]) {
 
             case "on":
                 send(true, program[pc]);
-                timeout = setTimeout(loop, program[pc++] * 1000);
+                timeout = next(loop.bind(null, next, send), program[pc++] * 1000);
                 break;
             case "off":
                 send(false, program[pc]);
-                timeout = setTimeout(loop, program[pc++] * 1000);
+                timeout = next(loop.bind(null, next, send), program[pc++] * 1000);
                 break;
             case "func":
                 while (program[pc++] !== 'end' && pc < program.length) {}
-                loop();
+                loop(next, send);
                 break;
             case "goto":
                 stack.push(pc + 1);
                 pc = program[pc];
-                loop();
+                loop(next, send);
                 break;
             case "end":
                 pc = stack.pop();
-                loop();
+                loop(next, send);
                 break;
-            case "stop":
+            case "stop":          
                 output.html('finished');
                 pc = 0;
                 break;
@@ -201,7 +82,7 @@ $(document).ready(function() {
     }
 
     function start() {
-        loop();
+        loop(setTimeout, send);
 
     }
 
@@ -224,6 +105,15 @@ $(document).ready(function() {
         reset();
         textarea.val(localStorage['commands']);
         input_update();
+    }
+
+    function run_sim(callback, timeout){
+    	simulation.push(timeout);
+    	callback();
+    }	
+
+    function sim_send(state){
+    	simulation.push(state);
     }
 
     function input_update() {
@@ -255,19 +145,30 @@ $(document).ready(function() {
         steps = getFloat(steps);
         for (var i = 0; i < steps; i++) {
             program.push('on');
-            console.log(on1, (on2 - on1), steps, i, i);
+            //console.log(on1, (on2 - on1), steps, i, i);
             program.push(on1 + (on2 - on1) / steps * i);
             program.push('off');
             program.push(off1 + (off2 - off1) / steps * i);
         }
     }
 
+    function getTemp(temp){
+
+    	for(var i=0; i < temp_map.length; i++){
+    		if(temp_map[i][0] > temp){
+    			return [temp_map[Math.max(i-1, 0)][1], temp_map[Math.max(i-1, 0)][2]];
+    		}
+    	}
+    	return [temp_map[temp_map.length-1][1], temp_map[temp_map.length-1][2]];
+    }
+
+
     function parse() {
         program = [];
+        simulation = [];
         var status = [];
         var funcs = [];
         var lines = textarea.val().split(/\r*\n/);
-
         lines.forEach(function(line, i) {
             var start_length = program.length;
             var tokens = line.replace(/^\s+|\s+$/g, '').split(/ +/);
@@ -311,6 +212,21 @@ $(document).ready(function() {
                     case 'tween':
                         [].push.apply(program, tween.apply(null, tokens.slice(1, tokens.length)));
                         break;
+                    case 'temp':
+                    	var pair = getTemp(getFloat(tokens[1]));
+                    	var duration = getFloat(tokens[2])*60;                 	
+                    	var length = pair[0] + pair[1];
+                    	for(; length < duration; length+=length){
+                    		[].push.apply(program, ['on', pair[0], 'off', pair[1]]);
+                    	}
+                    	break;
+                    case 'temp_tween':
+                    	var pair1 = getTemp(getFloat(tokens[1]));
+                    	var pair2 = getTemp(getFloat(tokens[2]));
+                    	var duration = getFloat(tokens[3])*60;  
+                    	var ave_duration = (pair1[0]+pair2[0])/2 + (pair1[1]-pair2[1])/2;
+                    	[].push.apply(program, tween(pair1[0], pair1[1], pair2[0], pair2[1], duration/ave_duration));
+                    	break;
                     case '':
                         break;
                     default:
@@ -328,6 +244,8 @@ $(document).ready(function() {
         });
         program.push('stop');
         console.log(program);
+        loop(run_sim, sim_send);
+		pc = 0;        
         output.html(status.join('<br/>'));
     }
 
@@ -474,4 +392,147 @@ $(document).ready(function() {
 
 
     update();
+
+
+   /* var coords = [];
+    var width = 700;
+    var height = 200;
+    var margin = {
+        top: 30,
+        right: 30,
+        bottom: 30,
+        left: 30
+    };
+
+
+    var lineFunc = d3.svg.line()
+        .x(function(d) {
+
+            return d.x;
+        })
+        .y(function(d) {
+            return d.y;
+        })
+        .interpolate("linear");
+
+    var xAxis = d3.svg.axis()
+        .scale(d3.scale.linear().range([0, width]))
+        .orient("bottom")
+        .ticks(5);
+    var yAxis = d3.svg.axis()
+        .scale(d3.scale.linear().range([height, 0]))
+        .orient("left")
+        .ticks(5);
+
+    var update_draw = function() {
+        coords = [];
+        d3.selectAll('.point')
+            .each(function(c) {
+                coords.push({
+                    x: this.x,
+                    y: this.y
+                });
+            });
+        coords.sort(function(a, b) {
+            return a.x - b.x;
+        });
+        svgContainer.selectAll('.path')
+            .data([coords]) // set the new data
+        .attr("d", lineFunc)
+    }
+
+    var click = function() {
+        if (d3.event.defaultPrevented) return;
+        var point = d3.mouse(this),
+            p = {
+                x: point[0],
+                y: point[1]
+            }
+        svgContainer.append("circle")
+            .attr("transform", "translate(" + p.x + "," + p.y + ")")
+            .attr("r", "6")
+            .attr('class', 'point')
+            .attr("stroke", "blue")
+            .attr("stroke-width", 1)
+            .attr("fill", "#3388ff")
+            .property({
+                x: p.x,
+                y: p.y
+            })
+            .style("cursor", "pointer")
+            .call(drag);
+        update_draw();
+    }
+
+
+
+    var svgContainer = d3.select("body")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style('padding', 30)
+        .on('click', click)
+
+
+    svgContainer.selectAll('.path')
+        .data([
+            []
+        ])
+        .enter().append('path')
+        .attr('class', 'path')
+        .attr('d', lineFunc)
+        .attr("stroke", "blue")
+        .attr("stroke-width", 2)
+        .attr("fill", "none");
+    svgContainer.append("g")
+        .attr("transform", "translate(0," + (height) + ")")
+        .attr({
+            fill: 'none',
+            stroke: 'black'
+        }).call(xAxis).selectAll('text')
+        .attr({
+            fill: 'black',
+            stroke: 'none'
+        });
+
+    svgContainer.append("g")
+        .attr("transform", "translate(0, " + 0 + ")")
+        .attr({
+            fill: 'none',
+            stroke: 'black'
+        }).call(yAxis).selectAll('text')
+        .attr({
+            fill: 'black',
+            stroke: 'none'
+        })
+
+
+    var drag = d3.behavior.drag()
+        .on("drag", dragmove);
+
+    function dragmove(d) {
+        var d3this = d3.select(this)
+        var index = d3this.attr('index') | 0;
+        var x = d3.event.x;
+        var y = d3.event.y;
+        d3this.attr("transform", "translate(" + x + "," + y + ")")
+            .property({
+                x: x,
+                y: y
+            });
+        update_draw();
+    }
+
+    var draw_sim = function(){
+    	var duration = simulation.reduce(function(memo, num) {
+    		if(!Number.isNaN(Number.parseInt(num))){
+    			return num + memo;
+    		}
+    		return memo;
+    	}, 0);
+    	return duration;
+
+    } */
+
+
 });
