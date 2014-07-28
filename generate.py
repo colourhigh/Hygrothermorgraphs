0771d0adf9262d1d8f9545781a94c821d734d904
Josh Gagnon <josh.n.gagnon@gmail.com>
2014-07-29 10:18:15 +1200

    now scanning for 3,4,5-grams.  Next up to replace word list with letter list

--- generate.py
+++ generate.py
@@ -5,13 +5,20 @@ from collections import defaultdict
 from os import listdir
 from os.path import isfile, join
 import pickle
-from itertools import permutations
 from random import choice
+
+
+def get_defaultdictint():
+    return defaultdict(int)
+
+def get_defaultdict():
+    return defaultdict(get_defaultdictint)
 
 with open('word_list.txt') as w:
     word_list = nltk.PunktWordTokenizer().tokenize(w.read().decode("utf-8"))
 word_set = set(word_list)
-n = 4
+ngrams = [3,4,5]
+
 try:
     model  = pickle.load( open( "model.pickle", "rb" ) )
 except:
@@ -21,25 +28,35 @@ except:
         for i in range(len(words) - (n-1)):
             yield words[i:i+n]
 
-    model = defaultdict(list)
+    model = defaultdict(get_defaultdict)
     directory = 'guten/unzipped'
-    for f in [ f for f in listdir(directory) if isfile(join(directory,f)) ]:
+    for f in [ f for f in listdir(directory) if isfile(join(directory,f))]:
         with open(join(directory,f)) as w:
             print f
-            for words in ngram(nltk.PunktWordTokenizer().tokenize(w.read().decode("latin-1")), n):
-                if len(word_set.intersection(words)) == n:
-                    model[tuple(words[:-1])].append(words[-1])
+            for n in ngrams:
+                for words in ngram(nltk.PunktWordTokenizer().tokenize(w.read().decode("latin-1")), n):
+                    if len(word_set.intersection(words)) == n:
+                        model[n][tuple(words[:-1])][words[-1]] +=1
     pickle.dump(model, open( "model.pickle", "wb" ) )
 
 
-def random_sentence():
+print model
+
+def expand_model(d):
+    result = []
+    for k in d:
+        result.extend([k] * d[k])
+    return result
+
+
+def random_sentence(n):
     def sentence(words, tup, max=45):
-        if len(words) > 45 or tup not in model:
+        if len(words) > 45 or tup not in model[n]:
             return words
-        rand = choice(model[tup])
+        rand = choice(expand_model(model[n][tup]))
         return sentence(words + ' ' + rand, tuple(list(tup[1:]) + [rand]))
-    start = choice(model.keys())
+    start = choice(model[n].keys())
     return sentence(' '.join(start), start)
 
 for i in range(100):
-    print random_sentence()
+    print random_sentence(3)
