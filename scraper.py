8a1dabda55b1f38d950b9956a9d92641bd3208c4
Josh Gagnon <josh.n.gagnon@gmail.com>
2014-08-18 14:14:35 +1200

    ramped up the scraper

--- scraper.py
+++ scraper.py
@@ -1,94 +1,163 @@
 from bs4 import BeautifulSoup
 import requests
 import string
+from textblob import TextBlob
+import nltk
+from nltk import word_tokenize
+from collections import defaultdict
+import json
+from pymongo import MongoClient
+from random import choice
+import datetime
+
+client = MongoClient()
+
+db = client['hygrothermographs']
+
+def get_defaultdictint():
+    return defaultdict(int)
+
+def get_defaultdict():
+    return defaultdict(get_defaultdictint)
+
+letters = set('AHIJLMNTUVYW '.lower())
+ngrams = [3,4,5]
+
+
+def ngram(words, n):
+    if len(words) < n:
+        return
+    for i in range(len(words) - (n-1)):
+        yield [w.lower() for w in words[i:i+n]]
+
+
+class Encoder(json.JSONEncoder):
+    def default(self, obj):
+        print obj
+        if isinstance(obj, defaultdict):
+            return dict(obj)
+        return obj
+
 
 class Spec(object):
-	host = ''
-	filename = ''
-	links = []
-	selectors = []
-	max = 1000
+    host = ''
+    links = []
+    selectors = []
+    max = 1000
 
-	def __init__(self):
-		self.links.append(self.host)
+    def __init__(self):
+        self.links.append(self.host)
 
-	def go(self):
-		pos = 0
-		results = []
-		while pos < len(self.links) and pos < self.max:
-			try:
-				res = requests.get(self.links[pos])
-				soup = BeautifulSoup(res.text)
-				count = 0
-				for link in soup.find_all('a'):
-					href = link.get('href')
-					if href:
-						if href.startswith('/'):
-							href = self.host + href
-						if href not in self.links and href.startswith(self.host):
-							self.links.append(href)
-							count += 1
-				print 'Found %d pages' % count
-				for selector in self.selectors:
-					for headline in soup.select(selector):
-						text = filter(lambda x: x in string.printable, headline.text.strip()).replace('\n', ' ')
-						if text not in results:
-							results.append(text)
-				with open(self.title_filename, 'w') as f:
-					f.write('\n'.join(results))
-				body = []
-				for b in soup.select(self.body_selector):
-					text= filter(lambda x: x in string.printable, b.text)
-					body.append(text)
+    def go(self):
+        pos = 0
+        self.titles = []
+        self.bodies = []
+        while pos < len(self.links) and pos < self.max:
+            try:
+                res = requests.get(self.links[pos])
+                soup = BeautifulSoup(res.text)
+                count = 0
+                for link in soup.find_all('a'):
+                    href = link.get('href')
+                    if href:
+                        if href.startswith('/'):
+                            href = self.host + href
+                        if href not in self.links and href.startswith(self.host):
+                            self.links= [href] + self.links
+                            count += 1
+                for selector in self.selectors:
+                    for headline in soup.select(selector):
+                        text = filter(lambda x: x in string.printable, headline.text.strip()).replace('\n', ' ')
+                        self.titles.append(text)
+                for b in soup.select(self.body_selector):
+                    text= filter(lambda x: x in string.printable, b.text)
+                    print text
+                    self.bodies.append(text)
+                print pos
+            except Exception, e:
+                print e
+            pos += 1
+        self.parse()
 
-				if len(body):
-					with open(self.body_filename, 'a') as f:
-						f.write('\n'.join(body))
-				print pos
-			except Exception, e:
-				print e
-			pos += 1
+    def parse(self):
+        text = filter(lambda x: x in string.printable, ' '.join(self.bodies))
+        #blob = TextBlob(text)
+        model = defaultdict(get_defaultdict)
+        for n in ngrams:
+            for words in ngram(nltk.PunktWordTokenizer().tokenize(text), n):
+                if set(''.join(words)).issubset(letters):
+                    model[n][tuple(words[:-1])][words[-1]] +=1
+        self.generate(model)
+
+
+    def generate(self, model):
+        results = []
+        def expand_model(d):
+            result = []
+            for k in d:
+                result.extend([k] * d[k])
+            return result
+
+        def random_sentence(n):
+            def sentence(words, tup, max=45):
+                if len(words) > 45 or tup not in model[n]:
+                    return words
+                rand = choice(expand_model(model[n][tup]))
+                return sentence(words + ' ' + rand, tuple(list(tup[1:]) + [rand]))
+            start = choice(model[n].keys())
+            return sentence(' '.join(start), start)
+        for i in range(100):
+            results.append(random_sentence(3))
+        self.save(set(results))
+
+    def save(self, data):
+        post = {
+            "name": self.name,
+            "trigrams": list(data),
+            "date": datetime.datetime.utcnow()
+        }
+        db.scrapings.insert(post)
 
 
 class Herald(Spec):
-	host = 'http://www.nzherald.co.nz'
-	title_filename = 'herald.txt'
-	body_filename = 'herald_body.txt'
-	links = []
-	selectors = ['h1', 'h3']
-	body_selector = '.articleBody p'
-	max =300
+    name = 'Herald'
+    host = 'http://www.nzherald.co.nz'
+    selectors = ['h1', 'h3']
+    body_selector = '#articleBody p'
+    max =300
 
 class Stuff(Spec):
-	host = 'http://www.stuff.co.nz'
-	title_filename = 'stuff.txt'
-	body_filename = 'stuff_body.txt'
-	links = []
-	selectors = ['h1', 'h2']
-	body_selector = '#left_col p'
-	max = 300
+    name = 'Stuff.co.nz'
+    host = 'http://www.stuff.co.nz'
+    selectors = ['h1', 'h2']
+    body_selector = '#left_col p'
+    max = 300
 
 class NYTimes(Spec):
-	host = 'http://www.nytimes.com'
-	title_filename = 'nytimes.txt'
-	body_filename = 'nytimes_body.txt'
-	links = []
-	selectors = ['h1', 'h2 a']
-	body_selector = '.story-content'
-	max = 300
+    name = 'NYTimes'
+    host = 'http://www.nytimes.com'
+    selectors = ['h1', 'h2 a']
+    body_selector = '.story-content'
+    max = 300
 
 class Guardian(Spec):
-	host = 'http://www.theguardian.com/uk'
-	title_filename = 'guardian.txt'
-	body_filename = 'guardian_body.txt'
-	links = []
-	selectors = ['h1', 'bullet a']
-	body_selector = '#article-body-blocks p'
-	max = 300
+    name = 'Guardian'
+    host = 'http://www.theguardian.com/uk'
+    selectors = ['h1', 'bullet a']
+    body_selector = '#article-body-blocks p'
+    max = 300
 
-#Guardian().go()
+class WashingtonPost(Spec):
+    name = 'Washington Post'
+    host = 'http://www.washingtonpost.com/'
+    selectors = ['h2']
+    body_selector = '#article-body p'
+    max = 300
+
+
+WashingtonPost().go()
+Guardian().go()
 Herald().go()
-
 Stuff().go()
-
 NYTimes().go()
+
